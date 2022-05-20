f1b2f919222b71e6c37e6d4d09e691e87174f132
chylvano <chylvanolandburg@gmail.com>
2022-05-20 11:15:12 +0200

    app naam + buttons neergezet en styling aangepast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,7 @@
 @extends('layouts.app')
+
+<button type="button" class="btn btn-secondary">Classical</button>
+<button type="button" class="btn btn-secondary">Country</button>
+<button type="button" class="btn btn-secondary">Rap</button>
+<button type="button" class="btn btn-secondary">Rock</button>
+<button type="button" class="btn btn-secondary">Pop</button>

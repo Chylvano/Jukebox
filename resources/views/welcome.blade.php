b649c9aa12885c9735f599ca140eb0ac0a169bac
chylvano <chylvanolandburg@gmail.com>
2022-12-07 09:54:43 +0100

    start dag 7-12-2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 <button type="button" class="btn btn-secondary">Country</button>
 <button type="button" class="btn btn-secondary">Hiphop</button>
 <button type="button" class="btn btn-secondary">Pop</button>
-<button type="button" href="{{ route('songs') }}" class="btn btn-secondary">Rock</button>
+<button type="button" class="btn btn-secondary">Rock</button>
+
 
 @endsection
